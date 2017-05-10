49074dbaa64a439a2893bb44ce46bed3500b6f90
bmwest <brianawest.m@gmail.com>
2017-05-10 10:18:29 -0400

    modifies method to account for total spending and 3 month  spending threshold

--- rewards.rb
+++ rewards.rb
@@ -1,32 +1,28 @@
-print "Please calculate your estimate with at least one month:\n"
-puts
-def total_rewards(bank_name, bank_pcb, month, monthly_spending, bonus = nil)
+def total_rewards(bank_name, bank_pcb, spending_threshold, month, monthly_spending, bonus = nil)
   bank_cbr = 0
+  total_spending = monthly_spending * month
   if bonus.nil? || month < 3
     bank_cbr += ((bank_pcb * monthly_spending) * month)
-  elsif !bonus.nil? && month >= 3
+  elsif !bonus.nil? && month >= 3 && total_spending >= spending_threshold
     bank_cbr += ((bank_pcb * monthly_spending) * month) + bonus
   end
-  if month == 1
-    "\nAfter spending $#{monthly_spending}/month for #{month} month, \nyou will have earned $#{bank_cbr.to_i} in cash back \nrewards with #{bank_name}."
-  else
-    "\nAfter spending $#{monthly_spending}/month for #{month} months, \nyou will have earned $#{bank_cbr.to_i} in cash back \nrewards with #{bank_name}."
-  end
+  "\nAfter spending $#{monthly_spending}/month for #{month} months, \nyou will have earned $#{bank_cbr.to_i} in cash back \nrewards with #{bank_name}."
+end
 
-puts total_rewards("Chase Bank", 0.01, 1, 1_300, 150)
-puts total_rewards("Chase Bank Unlimited", 0.015, 1, 1_300, 150)
+puts total_rewards("Chase Ink Business Cash", 0.01, 3_000, 1, 1_300, 300)
+puts total_rewards("Chase Bank Unlimited", 0.015, 500, 1, 1_300, 150)
 
 puts
 
-puts total_rewards("Chase Bank", 0.01, 6, 1_300, 150)
-puts total_rewards("Chase Bank Unlimited", 0.015, 6, 1_300, 150)
+puts total_rewards("Chase Ink Business Cash", 0.01, 3_000, 6, 1_300, 300)
+puts total_rewards("Chase Bank Unlimited", 0.015, 500, 6, 1_300, 150)
 
 puts
 
-puts total_rewards("Chase Bank", 0.01, 12, 1_300, 150)
-puts total_rewards("Chase Bank Unlimited", 0.015, 12, 1_300, 150)
+puts total_rewards("Chase Ink Business Cash", 0.01, 3_000, 12, 1_300, 300)
+puts total_rewards("Chase Bank Unlimited", 0.015, 500, 12, 1_300, 150)
 
 puts
 
-puts total_rewards("Chase Bank", 0.01, 32, 1_300, 150)
-puts total_rewards("Chase Bank Unlimited", 0.015, 32, 1_300, 150)
+puts total_rewards("Chase Ink Business Cash", 0.01, 3_000, 32, 1_300, 3--)
+puts total_rewards("Chase Bank Unlimited", 0.015, 500, 32, 1_300, 150)
